a89f3535049247075e9c43af6e41c01fc8b82db4
matheus <matheusxandre4@gmail.com>
2023-12-13 15:55:11 -0300

    iniciado a criação do html da avaliação, criação do repository da avaliação e foi adicionado o campo do menu da avaliacao.

---
+++ src/main/java/br/com/trocabeer/domain/repository/AvaliacaoRepository.java
@@ -0,0 +1,17 @@
+package br.com.trocabeer.domain.repository;
+
+import br.com.trocabeer.domain.model.Avaliacao;
+import br.com.trocabeer.domain.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, UUID> {
+
+	List<Avaliacao> findByMestreCervejeiroOrApreciador(Usuario mestreCervejeiro, Usuario apreciador);
+	List<Avaliacao> findByMestreCervejeiro(Usuario mestreCervejeiro);
+
+}
