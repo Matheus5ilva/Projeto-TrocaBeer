e39a3a18fefda841d79983d0db719e200e38cefc
matheus5ilva <matheusxandre4@gmail.com>
2023-12-13 00:14:33 -0300

    Criação da entidade avaliação. Alteração do texto Sobre nós, visão, missão, objetivo. Também alterado o application properties para o JPA criar a tabela.

--- src/main/java/br/com/trocabeer/domain/model/Avaliacao.java
+++ src/main/java/br/com/trocabeer/domain/model/Avaliacao.java
@@ -1,11 +1,39 @@
 package br.com.trocabeer.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
-public class Avaliacao {
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Avaliacao implements Serializable {
 
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @EqualsAndHashCode.Include
     private UUID avaliacaoId;
+    @ManyToOne
     private Usuario mestreCervejeiro;
+    @ManyToOne
     private Usuario apreciador;
+    @ManyToOne
+    private Troca troca;
+    @Column
+    @Size(min = 1, max = 5000)
+    private String avaliacao;
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "timestamp")
+    private OffsetDateTime dataCadastro;
 
 }
